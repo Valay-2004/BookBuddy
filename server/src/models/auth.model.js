4138ce72b921ea5b91f153eb16dd1721af09e8cd
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-09-13 17:13:58 +0000

    Created auth.model.js for find user and create user with async, and db.query with await

---
+++ server/src/models/auth.model.js
@@ -0,0 +1,22 @@
+const db = require('../config/database');
+
+async function findUserByEmail(email) {
+    const { rows } = await db.query(
+        'SELECT * FROM users WHERE email=$1',
+        [email]
+    );
+    return rows[0];
+}
+
+
+async function createUser({name, email, passwordHash, role = 'user' }) {
+    const { rows } = await db.query(
+        `INSERT INTO users (name, email, password_hash, role)
+        VALUES ($1, $2, $3, $4)
+        RETURNING id, name, email, role`,
+        [name, email, passwordHash, role]
+    );
+    return row[0];
+}
+
+module.exports = { findUserByEmail, createUser };
