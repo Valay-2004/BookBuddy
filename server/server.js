09be96f896ceea8042acb44cb8b1e42c6cebe9bf
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-09-14 11:48:57 +0000

    Added routes and server.js (express) for backend routing

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+const authRoutes = require('./src/routes/auth.routes');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+
+app.listen(5000, () => console.log('Server running on port 5000'));
