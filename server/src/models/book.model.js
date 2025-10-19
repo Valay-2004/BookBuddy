04739fd6c412469b4c8307555dcdc8b092e067f0
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-10-19 16:26:59 +0000

    Changed two lines in one -- added both functions in one export line

--- server/src/models/book.model.js
+++ server/src/models/book.model.js
@@ -1,16 +1,16 @@
-const db = require('../config/database');
+const db = require("../config/database");
 
 async function getAllBooks() {
-    const { rows } = await db.query('SELECT * FROM books');
-    return rows;
+  const { rows } = await db.query("SELECT * FROM books");
+  return rows;
 }
 
 async function createBook(title, author, description) {
-    const { rows } = await db.query('INSERT INTO books (title, author, description) VALUES ($1, $2, $3) RETURNING *',
-        [title, author, description]
-    );
-    return rows[0];
+  const { rows } = await db.query(
+    "INSERT INTO books (title, author, description) VALUES ($1, $2, $3) RETURNING *",
+    [title, author, description]
+  );
+  return rows[0];
 }
 
-module.exports = { getAllBooks };
-module.exports = { createBook };
+module.exports = { createBook, getAllBooks };
