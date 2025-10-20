6546ae64fe5b4456ab4b53744e0c0e97011a0233
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-10-20 15:04:43 +0000

    Added getReviewsForBook function and addReview

--- server/src/models/review.model.js
+++ server/src/models/review.model.js
@@ -13,3 +13,15 @@ async function addReview({ userId, bookId, rating, reviewText }) {
   );
   return rows[0];
 }
+
+// create a getReviews for book function
+async function getReviewsForBook(bookId) {
+  const { rows } = await db.query(
+    `SELECT r.id, r.rating, r.review_text, r.created_at, u.name AS reviewer_name
+    FROM reviews r
+    JOIN users u ON r.user_id = u_id
+    WHERE r.book_id = $1
+    ORDER BY r.created_at DESC`,
+    [bookId]
+  );
+}
