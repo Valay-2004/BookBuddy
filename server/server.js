2cefd270fda14a35e0cc4385143c7afe059d0e5d
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-10-22 13:35:37 +0000

    Added reviewRoutes with /api

--- server/server.js
+++ server/server.js
@@ -1,14 +1,16 @@
-const express = require('express');
-const cors = require('cors');
-const authRoutes = require('./src/routes/auth.routes');
-const bookRoutes = require('./src/routes/book.routes');
+const express = require("express");
+const cors = require("cors");
+const authRoutes = require("./src/routes/auth.routes");
+const bookRoutes = require("./src/routes/book.routes");
+const reviewRoutes = require("./src/routes/review.routes");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-app.use('/api/books', bookRoutes);
-
-app.use('/api/auth', authRoutes);
-
-app.listen(5000, () => console.log('Server running on port 5000'));
+// for bookRoutes
+app.use("/api/books", bookRoutes);
+// for authRoutes
+app.use("/api/auth", authRoutes);
+// for reviews routes
+app.use("/api", reviewRoutes);
+app.listen(5000, () => console.log("Server running on port 5000"));
