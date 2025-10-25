b93dfa8fc773789105d01308f0555b1fa74fc5d7
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-10-25 11:10:35 +0000

    Added routes for admin actions

---
+++ server/src/routes/admin.routes.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const { authorizeRole } = require("../middleware/auth");
+const authenticate = require("../middleware/auth");
+const {
+  createBook,
+  deleteBook,
+  getAllReviews,
+  deleteReview,
+} = require("../controllers/admin.controller");
+
+const router = express.Router();
+
+// Books management
+router.post("/admin/books", authenticate, authorizeRole("admin"), createBook);
+router.delete(
+  "/admin/books/:id",
+  authenticate,
+  authorizeRole("admin"),
+  deleteBook
+);
+
+// Review moderation
+router.get(
+  "/admin/reviews",
+  authenticate,
+  authorizeRole("admin", getAllReviews)
+);
+router.delete(
+  "/admin/reviews/:id",
+  authenticate,
+  authorizeRole("admin"),
+  deleteReview
+);
+
+module.exports = router;
