63065b14dc0f6e8162cb80b593e57e4e08598cc2
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-10-29 15:46:31 +0000

    Creating AuthProvider --> global state for token + user

---
+++ client/src/context/AuthContext.jsx
@@ -0,0 +1,8 @@
+import { createContext, useContent, useState, useEffect } from "react";
+
+const AuthContext = createContext();
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(localStorage.getItem("token"));
+}
