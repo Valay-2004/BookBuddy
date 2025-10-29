c1ca6408b74bac3bdadb9964bf49de23d2319f69
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-10-29 15:43:34 +0000

    Added auth service for login and signup

---
+++ client/src/services/auth.js
@@ -0,0 +1,13 @@
+import API from "./api";
+
+// Signup
+export async function signup(userData) {
+  const response = await API.post("/auth/signup", userData);
+  return response.data;
+}
+
+// Login
+export async function login(credentials) {
+  const response = await API.post("/auth/log", credentials);
+  return response.data;
+}
