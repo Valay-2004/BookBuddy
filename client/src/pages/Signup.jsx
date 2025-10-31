3604b8eb3a50335cca64ecc495eb46c7e3e0fff7
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-10-31 11:04:42 +0000

    Added Signup logic and UI

---
+++ client/src/pages/Signup.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import { signup } from "../services/auth";
+import { useNavigate } from "react-router-dom";
+const signupIcon = "../assets/signup.png";
+
+export default function Signup() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [address, setAddress] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      await signup({ name, email, password, address });
+      navigate("/login");
+    } catch (err) {
+      setError("Signup Failed - maybe user already exists?");
+    }
+  }
+
+  // return the page
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h2>
+        <img src={signupIcon} alt="Signup" /> SignUp
+      </h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />{" "}
+        <br />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />{" "}
+        <br />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <br />
+        <input
+          type="text"
+          placeholder="address"
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+        />{" "}
+        <br />
+        <button type="submit">Signup</button>
+      </form>
+      {error && <p style={{ color: "red" }}>{error}</p>}
+    </div>
+  );
+}
