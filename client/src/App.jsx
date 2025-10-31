7cef620d02cf7fd9217bd5e702b7028b9aca544c
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-10-31 11:19:39 +0000

    Added routes for Login, Signup, Profile

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,22 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Books from "./pages/Books";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
     <Router>
-      <nav style={{ padding: "1rem", background: "#f0f0f0" }}>
-        <Link to="/">Home</Link>
+      <nav style={{ padding: "1rem", background: "#eee" }}>
+        <Link to="/">Books</Link> |<Link to="/login">Login</Link> |
+        <Link to="/signup">Signup</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<Books />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
   );
