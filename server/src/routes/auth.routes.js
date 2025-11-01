714e2d8f7094560c2e52dde9141a1919ace12d21
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-11-01 12:35:37 +0000

    Added Input validation for login and signup

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,9 +1,21 @@
-const express = require('express');
-const { signup, login } = require('../controllers/auth.controller');
-
+const express = require("express");
+const { body } = require("express-validator");
+const { signup, login } = require("../controllers/auth.controller");
 const router = express.Router();
 
-router.post('/signup', signup);
-router.post('/login', login);
+router.post(
+  "/signup",
+  [
+    body("name").isLength()({ min: 3 }.trim().escape()),
+    body("email").isEmail().normalizeEmail(),
+    body("password").isLength({ min: 8 }),
+  ],
+  signup
+);
+router.post(
+  "/login",
+  [body("email").isEmail().normalizeEmail(), body("password").notEmpty()],
+  login
+);
 
 module.exports = router;
