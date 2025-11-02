71c59008d806e50591ba0ef4487e2d70eb792958
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-11-02 14:11:22 +0000

    It was a long lasting bug but at last fixed it and added the admin authorizeRole

--- server/src/routes/book.routes.js
+++ server/src/routes/book.routes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const { listBooks, addBook } = require("../controllers/book.controller");
 const authenticate = require("../middleware/auth");
-
+const authorizeRole = require("../middleware/authorizeRole");
 const router = express.Router();
 
 // Public route
 router.get("/", listBooks);
 
 // Protected route
-router.post("/", authenticate, addBook);
+router.post("/", authenticate, authorizeRole("admin"), addBook);
+
 module.exports = router;
