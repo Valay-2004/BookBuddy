f5e373e6988b6642ac3bfaafd3987e26a9938c50
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-11-10 15:07:00 +0000

    Login page beautification

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -31,7 +31,7 @@ export default function Login() {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-indigo-50 to-blue-100 dark:from-gray-900 dark:to-indigo-950 p-4">
+    <div className="min-h-screen flex items-center justify-center bg-linear-to-r from-indigo-50 to-blue-100 dark:from-gray-900 dark:to-indigo-950 p-4">
       <motion.div
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
