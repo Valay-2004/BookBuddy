f7673383895558e68369e9273b129cc433845f4f
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-11-12 14:49:29 +0000

    Attached token from localStorage for convenience

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -9,4 +9,17 @@ const API = axios.create({
   },
 });
 
+// Attach token from localStorage for convenience
+API.interceptors.request.use((config) => {
+  try {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  } catch (err) {
+    // ignore
+  }
+  return config;
+});
+
 export default API;
