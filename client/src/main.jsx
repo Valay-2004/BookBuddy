7f92d7de7b403d647b868f00e1f78da07a4dec6a
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2026-01-07 12:32:14 +0000

    OLD COMMMITS: these are old updates forgot to commit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDom from "react-dom/client";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
+import { ThemeProvider } from "./context/ThemeContext.jsx";
 import "./index.css";
 
 ReactDom.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </AuthProvider>
   </React.StrictMode>
 );
